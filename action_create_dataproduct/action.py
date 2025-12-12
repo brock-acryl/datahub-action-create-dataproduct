cbe67557b947fa0f0180e24f9a94fc132b36926f
Brock Griffey <52086127+brock-acryl@users.noreply.github.com>
2025-12-11 21:24:27 -0500

    Refactor logging in CreateDataproductAction to use logger instead of print statements for better logging practices.

--- action_create_dataproduct/action.py
+++ action_create_dataproduct/action.py
@@ -1,19 +1,22 @@
 from datahub_actions.action.action import Action
 from datahub_actions.event.event_envelope import EventEnvelope
 from datahub_actions.pipeline.pipeline_context import PipelineContext
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class CreateDataproductAction(Action):
     @classmethod
     def create(cls, config_dict: dict, ctx: PipelineContext) -> "Action":
-        print(config_dict)
+        logger.info(config_dict)
         return cls(ctx)
 
     def __init__(self, ctx: PipelineContext):
         self.ctx = ctx
 
     def act(self, event: EventEnvelope) -> None:
-        print(event)
+        logger.info(event)
 
     def close(self) -> None:
         pass
